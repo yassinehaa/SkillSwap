ac538f92f4e0532de42d9d8070537966a8b9ac69
yassinaha <hayassine0@gmail.com>
2025-06-14 14:56:58 +0100

    first push

---
+++ SkillSwapBackEnd/src/main/java/org/skillswap/skillswapbackend/SkillSwapBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.skillswap.skillswapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSwapBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillSwapBackEndApplication.class, args);
+    }
+
+}
---
+++ SkillSwapBackEnd/src/test/java/org/skillswap/skillswapbackend/SkillSwapBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skillswap.skillswapbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSwapBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
